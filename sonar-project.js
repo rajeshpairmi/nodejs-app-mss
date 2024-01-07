056613e30c26899c8d98ad060bb300d9f115bb9f
Venkata Rajesh Parimi <62972262+rajeshpairmi@users.noreply.github.com>
2024-01-07 02:39:18 -0800

    Update sonar-project.js
    mistake

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.125.206.150/:9000/',
+  serverUrl: 'http://34.125.206.150:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
